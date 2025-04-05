c2af13fd979c016c0846d8383726f2323c75930c
Nick Savage <nick@nicksavage.ca>
2025-04-05 18:53:20 -0400

    working on a backtracking implementation

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,46 @@
 from collections import defaultdict
+from typing import List
+
+class Position:
+    def __init__(self, row, col, number, state):
+        self.row = row
+        self.col = col
+        self.number = number
+        self.state = state
 
 class Board:
     def __init__(self, state):
         self.state = state
         self.options = [[[] for _ in range(9)] for _ in range(9)]
+        self.rejected = [[[] for _ in range(9)] for _ in range(9)]
+        self.guesses: List[Position] = []
+        self.no_match = False
+        self.complete = False
+        self.inconsistency = False
+
+sudoku_hard = [
+    [0, 0, 0, 6, 0, 0, 4, 0, 0],
+    [7, 0, 0, 0, 0, 3, 6, 0, 0],
+    [0, 0, 0, 0, 9, 1, 0, 8, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 5, 0, 1, 8, 0, 0, 0, 3],
+    [0, 0, 0, 3, 0, 6, 0, 4, 5],
+    [0, 4, 0, 2, 0, 0, 0, 6, 0],
+    [9, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 1, 0, 0]
+]
+
+sudoku_medium = [
+    [0, 0, 0, 2, 6, 0, 7, 0, 1],
+    [6, 8, 0, 0, 7, 0, 0, 9, 0],
+    [1, 9, 0, 0, 0, 4, 5, 0, 0],
+    [8, 2, 0, 1, 0, 0, 0, 4, 0],
+    [0, 0, 4, 6, 0, 2, 9, 0, 0],
+    [0, 5, 0, 0, 0, 3, 0, 2, 8],
+    [0, 0, 9, 3, 0, 0, 0, 7, 4],
+    [0, 4, 0, 0, 5, 0, 0, 3, 6],
+    [7, 0, 3, 0, 1, 8, 0, 0, 0]
+]
 
 sudoku_board = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -17,18 +54,6 @@ sudoku_board = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
-complete_sudoku_board = [
-    [5, 3, 4, 6, 7, 8, 9, 1, 2],
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
-
 ROWS = 9
 COLS = 9
 
@@ -37,27 +62,29 @@ def generate_cell_options(board, row, col) -> list:
         print("pos already filled")
         return []
     options = []
+
     freq = defaultdict(int)
+    square_row = int(row / 3)
+    square_col = int(col / 3)
+
     for col_index in range(COLS):
         item = board.state[row][col_index]
         freq[item] += 1
     for row_index in range(ROWS):
         item = board.state[row_index][col]
         freq[item] += 1
-    square_row = int(row / 3)
-    square_col = int(col / 3)
+
     for col_index in range(3):
         for row_index in range(3):
             x = square_row * 3 + row_index
             y = square_col * 3 + col_index
-            item = board.state[x][y]
-            print(row, col, square_row, square_col, x, y, item)
-            freq[item] += 1
+            freq[board.state[x][y]] += 1
 
     for number in range(0,10):
         if freq[number] == 0:
             if item not in options:
-                options.append(number)
+                if number not in board.rejected[row][col]:
+                    options.append(number)
     return options
 
 def find_options(board) -> None:
@@ -75,7 +102,6 @@ def is_solved(board) -> bool:
                 return False
     for row_index in range(ROWS):
         freq = defaultdict(int)
-        print(f"check row {row_index}")
         for col_index in range(COLS):
             item = board[row_index][col_index]
             freq[item] += 1
@@ -85,7 +111,7 @@ def is_solved(board) -> bool:
 
     for col_index in range(COLS): # s/b different
         freq = defaultdict(int)
-        print(f"check col {col_index}")
+        #print(f"check col {col_index}")
         for col_index in range(ROWS):
             item = board[row_index][col_index]
             freq[item] += 1
@@ -109,40 +135,84 @@ def is_solved(board) -> bool:
 
     return True
 
-assert is_solved(sudoku_board) == False
-assert is_solved(complete_sudoku_board)
-board = Board(sudoku_board)
-
-for row in board.state:
-    print(row)
-
-while True:
+def forward_pass(board) -> Board:
     find_options(board)
-
-    for row in board.options:
-        print(row)
-
-    print()
     change = False
     for row in range(ROWS):
         for col in range(COLS):
             options = board.options[row][col]
+            
+            # inconsistency
+            if board.state[row][col] == 0 and len(options) == 0:
+                for x in board.options:
+                    print(x)
+                for x in board.state:
+                    print(x)
+                print(f"inconsistency at {row},{col}")
+                board.inconsistency = True
+                return board
+
             if len(options) == 1:
-                print(f"changed {row} {col}")
+#                print(f"changed {row} {col}")
                 board.state[row][col] = board.options[row][col][0]
                 board.options[row][col] = []
                 change = True
-                break
         if change:
             break
     if not change:
         print("no change made")
-        break
+        board.no_match = True
+        return board
 
-    for row in board.state:
-        print(row)
     if is_solved(board.state):
-        print("solved!")
-        break 
+        board.complete = True
+    return board
+
+board = Board(sudoku_hard)
+
+def run_state(board) -> Board:
+    complete = False
+    while not complete:
+        board = forward_pass(board)
+        complete = board.complete
+
+        if board.inconsistency:
+            board.inconsistency = False
+            print(board)
+            if len(board.guesses) > 0:
+                last_pos = board.guesses.pop()
+                print(f"inconsistency: {last_pos.number} at {last_pos.row},{last_pos.col}")
+                board.state = last_pos.state
+                board.rejected[last_pos.row][last_pos.col].append(last_pos.number)
+            else:
+                board.no_match = True
+
+        if board.no_match:
+            for row in range(ROWS):
+                for col in range(COLS):
+                    if len(board.options[row][col]) == 0:
+                        continue
+                    print(board.options[row][col])
+                    for option in board.options[row][col]:
+                        if option in board.rejected[row][col]:
+                            continue
+                        board.no_match = False
+                        pos = Position(row, col, option, board.state)
+                        board.state[row][col] = option
+                        print(f"lets guess at {option} at {row},{col}")
+                        board.guesses.append(pos)
+                        board = run_state(board)
+                        break
+                if not board.no_match:
+                    break
+            if not board.no_match:
+                break
+
+
+            print("can't find a match")
+            break
+
+run_state(board)
+
 for row in board.state:
     print(row)
